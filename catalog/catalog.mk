d1d95833cf42ba3f55a1af65dd08ed3d07730823
Alexandre Carbenay <acarbenay@adhuc.fr>
2024-09-25 13:57:59 +0200

    chore: Build docker image during github workflow

--- catalog/catalog.mk
+++ catalog/catalog.mk
@@ -1,7 +1,8 @@
 gradleCatalog = $(CATALOG_LOCATION)/gradlew -p $(CATALOG_LOCATION)
 
 build-catalog: ## Build the catalog application
-	$(gradleCatalog) check bootJar
+	$(gradleCatalog) check bootJar && \
+		docker build -t catalog:latest --build-arg REVISION=$(REVISION) --build-arg VERSION=$(VERSION) --build-arg CREATION_DATE=$(shell date --utc --iso-8601=seconds) $(CATALOG_LOCATION)
 
 clean-catalog: ## Clean the catalog project folder
 	$(gradleCatalog) clean
