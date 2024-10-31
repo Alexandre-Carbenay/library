ba291d1b30ebc5d973989609961529be49646e3d
Alexandre Carbenay <acarbenay@adhuc.fr>
2024-10-31 13:20:42 +0100

    doc: Add README file with additional targets to start and stop applications

--- catalog/catalog.mk
+++ catalog/catalog.mk
@@ -1,8 +1,29 @@
+# Define environment variables for targets executions (excluding docker compose), and override from .env if exist
+CATALOG_API_PORT=8080
+
+# Prepare environment variables for docker compose from .env if exist, using --env-file parameter
+INCLUDE_ENV=
+ifneq (,$(wildcard $(ENV_LOCATION)/.env))
+	INCLUDE_ENV=--env-file $(ENV_LOCATION)/.env
+endif
+
 gradleCatalog = $(CATALOG_LOCATION)/gradlew -p $(CATALOG_LOCATION)
 
 build-catalog: ## Build the catalog application
 	$(gradleCatalog) check bootJar && \
 		docker build -t catalog:latest --build-arg REVISION=$(REVISION) --build-arg VERSION=$(VERSION) --build-arg CREATION_DATE=$(shell date --utc --iso-8601=seconds) $(CATALOG_LOCATION)
+
+start-catalog: ## Start the catalog application
+	docker compose $(INCLUDE_ENV) \
+		-f $(CATALOG_LOCATION)/docker/docker-compose.yml \
+		-f $(CATALOG_LOCATION)/docker/docker-compose.port.yml \
+		up -d
+
+stop-catalog: ## Stop the catalog application
+	docker compose $(INCLUDE_ENV) \
+		-f $(CATALOG_LOCATION)/docker/docker-compose.yml \
+		-f $(CATALOG_LOCATION)/docker/docker-compose.port.yml \
+		down
 
 acceptance-catalog: ## Run acceptance tests on the catalog application
 	$(gradleCatalog) acceptance
