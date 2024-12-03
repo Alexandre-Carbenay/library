b129d40b719c11b3bdd6d9ca335a0c2d59ff6923
Alexandre Carbenay <acarbenay@adhuc.fr>
2024-12-03 13:45:32 +0100

    chore: Fix deprecations from Spring Boot 3.4.0

--- website/src/test/java/org/adhuc/library/website/catalog/CatalogControllerTests.java
+++ website/src/test/java/org/adhuc/library/website/catalog/CatalogControllerTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -22,7 +22,7 @@ class CatalogControllerTests {
 
     @Autowired
     private MockMvc mvc;
-    @MockBean
+    @MockitoBean
     private CatalogClient catalogClient;
 
     @Test
