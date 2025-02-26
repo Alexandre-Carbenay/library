728b8219fe48b8e50929d00ac05710f608f10a59
Alexandre Carbenay <acarbenay@adhuc.fr>
2025-02-26 17:34:28 +0100

    chore: Adapt CD pipeline for catalog to publish docker image to GH registry
    The CD pipeline is split in 3 different phases:
    - build: build the spring bootable JAR and include it into a docker image, published to the GH container registry
    - acceptance: retrieve previously built docker image and execute acceptance tests suite on it
    - release: create a release based on the docker image (to be continued)
    
    We still want the CD pipeline to use the make targets to ensure that both local and CD builds use the same steps to build the artifact

--- catalog/catalog.mk
+++ catalog/catalog.mk
@@ -9,11 +9,24 @@ endif
 
 gradleCatalog = $(CATALOG_LOCATION)/gradlew -p $(CATALOG_LOCATION)
 
-build-catalog: ## Build the catalog application
+build-catalog: build-catalog-compile build-catalog-test build-catalog-jar build-catalog-docker ## Build the catalog application
+
+build-catalog-compile: ## Compile the catalog application
+	$(gradleCatalog) classes
+
+build-catalog-test: ## Run tests on the catalog application
 	$(MAKE) start-pact-broker
-	$(gradleCatalog) check bootJar && \
-		docker build -t catalog:latest --build-arg REVISION=$(REVISION) --build-arg VERSION=$(VERSION) --build-arg CREATION_DATE=$(shell date --utc --iso-8601=seconds) $(CATALOG_LOCATION)
+	$(gradleCatalog) check
 	$(MAKE) stop-pact-broker
+
+build-catalog-test-publish: ## Publish tests report for the catalog application
+	# For now, we don't publish anything
+
+build-catalog-jar: ## Build JAR for the catalog application
+	$(gradleCatalog) bootJar
+
+build-catalog-docker: ## Build Docker image for the catalog application
+	docker build -t catalog:latest --build-arg REVISION=$(REVISION) --build-arg VERSION=$(VERSION) --build-arg CREATION_DATE=$(shell date --utc --iso-8601=seconds) $(CATALOG_LOCATION)
 
 start-catalog: ## Start the catalog application
 	docker compose $(INCLUDE_ENV) \
