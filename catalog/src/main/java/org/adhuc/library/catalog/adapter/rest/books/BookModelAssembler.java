2cbdb9002ef1701fb250d9f5ea569c2f99b08e48
Alexandre Carbenay <acarbenay@adhuc.fr>
2025-04-11 16:45:29 +0200

    fix: Provide catalog filtering books in accept language, whatever the original language

--- catalog/src/main/java/org/adhuc/library/catalog/adapter/rest/books/BookModelAssembler.java
+++ catalog/src/main/java/org/adhuc/library/catalog/adapter/rest/books/BookModelAssembler.java
@@ -1,9 +1,13 @@
 package org.adhuc.library.catalog.adapter.rest.books;
 
 import org.adhuc.library.catalog.books.Book;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -17,8 +21,14 @@ public class BookModelAssembler extends RepresentationModelAssemblerSupport<Book
     @NonNull
     @Override
     public BookModel toModel(@NonNull Book book) {
-        var model = instantiateModel(book);
+        return toModel(book, book.originalLanguage());
+    }
+
+    @NonNull
+    public BookModel toModel(@NonNull Book book, @NonNull String language) {
+        var model = instantiateModel(book, language);
         model.add(linkTo(methodOn(BooksController.class).getBook(book.id(), null)).withSelfRel());
+        book.wikipediaLinkIn(language).ifPresent(link -> model.add(Link.of(link.value()).withRel(link.source())));
         return model;
     }
 
@@ -26,5 +36,18 @@ public class BookModelAssembler extends RepresentationModelAssemblerSupport<Book
     @Override
     protected BookModel instantiateModel(@NonNull Book book) {
         return new BookModel(book);
+    }
+
+    @NonNull
+    protected BookModel instantiateModel(@NonNull Book book, @NonNull String language) {
+        return new BookModel(book, language);
+    }
+
+    public CollectionModel<BookModel> toCollectionModel(Iterable<Book> books, String language) {
+        var resources = new ArrayList<BookModel>();
+        for (var book : books) {
+            resources.add(toModel(book, language));
+        }
+        return CollectionModel.of(resources);
     }
 }
