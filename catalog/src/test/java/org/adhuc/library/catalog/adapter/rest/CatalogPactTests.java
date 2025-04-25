c168295873290f6c203cc70bb6a70fd8beee37b9
Alexandre Carbenay <acarbenay@adhuc.fr>
2025-04-25 13:01:19 +0200

    test: Add pact on catalog for book detail

--- catalog/src/test/java/org/adhuc/library/catalog/adapter/rest/CatalogPactTests.java
+++ catalog/src/test/java/org/adhuc/library/catalog/adapter/rest/CatalogPactTests.java
@@ -1,4 +1,4 @@
-package org.adhuc.library.catalog.adapter.rest.catalog;
+package org.adhuc.library.catalog.adapter.rest;
 
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
@@ -7,11 +7,10 @@ import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import org.adhuc.library.catalog.authors.Author;
-import org.adhuc.library.catalog.books.Book;
-import org.adhuc.library.catalog.books.CatalogService;
-import org.adhuc.library.catalog.books.ExternalLink;
-import org.adhuc.library.catalog.books.LocalizedDetails;
+import org.adhuc.library.catalog.books.*;
+import org.adhuc.library.catalog.editions.Edition;
 import org.adhuc.library.catalog.editions.EditionsService;
+import org.adhuc.library.catalog.editions.PublicationDate;
 import org.apache.hc.core5.http.HttpRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
@@ -24,6 +23,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -34,12 +34,14 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Provider("library-catalog")
 @PactBroker(url = "http://localhost:9292")
-class CatalogControllerPactTests {
+class CatalogPactTests {
 
     @LocalServerPort
     int port;
     @MockitoBean
     private CatalogService catalogService;
+    @MockitoBean
+    private BooksService booksService;
     @MockitoBean
     private EditionsService editionsService;
 
@@ -137,6 +139,40 @@ class CatalogControllerPactTests {
 
         var request = PageRequest.of(1, 25);
         when(catalogService.getPage(request, FRENCH)).thenReturn(new PageImpl<>(List.of(book), request, 67));
+    }
+
+    @State("Book detail is reachable")
+    void bookDetail() {
+        var bookId = UUID.fromString("b6608a30-1e9b-4ae0-a89d-624c3ca85da4");
+        var book = new Book(
+                bookId,
+                Set.of(new Author(
+                        UUID.fromString("99287cef-2c8c-4a4d-a82e-f1a8452dcfe2"),
+                        "Jean-Jacques Rousseau",
+                        LocalDate.parse("1712-06-28"),
+                        LocalDate.parse("1778-07-02")
+                )),
+                "fr",
+                Set.of(new LocalizedDetails(
+                        "fr",
+                        "Du contrat social",
+                        "Du contrat social est un traité de philosophie politique présentant ...",
+                        Set.of(
+                                new ExternalLink("wikipedia", "https://fr.wikipedia.org/wiki/Du_contrat_social")
+                        )
+                ))
+        );
+        var edition = new Edition(
+                "9782290385050",
+                "Du contrat social ou Principes du droit politique",
+                PublicationDate.of(LocalDate.parse("2023-02-08")),
+                book,
+                "fr",
+                "Comment se forment les sociétés ? Qu'est-ce qui détermine une organisation sociale juste ? Comment concilier les intérêts de chacun et l'intérêt général ?"
+        );
+
+        when(booksService.getBook(bookId)).thenReturn(Optional.of(book));
+        when(editionsService.getBookEditions(bookId)).thenReturn(List.of(edition));
     }
 
 }
