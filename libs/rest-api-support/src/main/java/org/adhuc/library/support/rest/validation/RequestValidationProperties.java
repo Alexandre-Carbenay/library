b3cac6bc6d21bfedfd104fe340280c131f78ca67
Alexandre Carbenay <acarbenay@adhuc.fr>
2025-12-17 09:15:41 +0100

    fix: Enforce default config on REST API support library auto-config

--- libs/rest-api-support/src/main/java/org/adhuc/library/support/rest/validation/RequestValidationProperties.java
+++ libs/rest-api-support/src/main/java/org/adhuc/library/support/rest/validation/RequestValidationProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "org.adhuc.library.support.rest.validation", ignoreUnknownFields = false)
-public record RequestValidationProperties(OpenApi openApi) {
+public record RequestValidationProperties(@DefaultValue OpenApi openApi) {
 
     public record OpenApi(@DefaultValue("classpath:api/openapi.yml") String location) {
     }
