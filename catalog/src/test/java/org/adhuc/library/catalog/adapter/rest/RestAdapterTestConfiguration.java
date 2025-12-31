8cc61c4f891ed9b432b1ca99ac95e845db1d7cbd
Alexandre Carbenay <acarbenay@adhuc.fr>
2025-12-31 15:59:04 +0100

    refactor(catalog): Use pagination serialization from rest-api-support lib

--- catalog/src/test/java/org/adhuc/library/catalog/adapter/rest/RestAdapterTestConfiguration.java
+++ catalog/src/test/java/org/adhuc/library/catalog/adapter/rest/RestAdapterTestConfiguration.java
@@ -1,12 +1,11 @@
 package org.adhuc.library.catalog.adapter.rest;
 
+import org.adhuc.library.support.rest.pagination.PaginationAutoConfiguration;
 import org.adhuc.library.support.rest.validation.RequestValidationAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Import;
 
 @TestConfiguration
-@ImportAutoConfiguration(RequestValidationAutoConfiguration.class)
-@Import(PaginationSerializationConfiguration.class)
+@ImportAutoConfiguration({RequestValidationAutoConfiguration.class, PaginationAutoConfiguration.class})
 public class RestAdapterTestConfiguration {
 }
