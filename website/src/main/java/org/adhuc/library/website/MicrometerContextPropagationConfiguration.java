5f97fec697c0dfc05bcda9b2aa3495a4b860da50
Alexandre Carbenay <acarbenay@adhuc.fr>
2025-12-31 14:23:49 +0100

    fix: Restore tracing capabilities on website and catalog

---
+++ website/src/main/java/org/adhuc/library/website/MicrometerContextPropagationConfiguration.java
@@ -0,0 +1,23 @@
+package org.adhuc.library.website;
+
+import io.micrometer.context.ContextExecutorService;
+import io.micrometer.context.ContextSnapshotFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+/**
+ * This configuration ensures that the Resilience4j circuit breaker and Spring RestClient use the micrometer tracing
+ * context to enhance the requests to APIs.
+ */
+@Configuration
+class MicrometerContextPropagationConfiguration {
+
+    MicrometerContextPropagationConfiguration(Resilience4JCircuitBreakerFactory circuitBreakerFactory) {
+        circuitBreakerFactory.configureExecutorService(
+                ContextExecutorService.wrap(Executors.newCachedThreadPool(), ContextSnapshotFactory.builder().build())
+        );
+    }
+
+}
